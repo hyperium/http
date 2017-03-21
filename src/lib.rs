250d9334fc242b04642aaed294c716c17143314d
Carl Lerche <me@carllerche.com>
2017-03-21 11:31:53 -0700

    Initial implementation of Uri (#10)
    An initial implementation of URIs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+extern crate bytes;
+
 pub mod method;
 pub mod status;
 pub mod version;
+pub mod uri;
+
+mod byte_str;
 
 pub use method::Method;
 pub use status::StatusCode;
 pub use version::Version;
+pub use uri::Uri;
