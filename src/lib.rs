0858e716984c95d2dd0c04f57a228e324ebc0f41
Carl Lerche <me@carllerche.com>
2017-05-31 21:40:47 -0700

    Initial implementation of `Request` (#22)
    This is an initial implementation of `Request`. It is conservative and further improvements are expected.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate fnv;
 
 pub mod header;
 pub mod method;
+pub mod request;
 pub mod status;
 pub mod version;
 pub mod uri;
@@ -11,6 +12,7 @@ mod byte_str;
 
 pub use header::HeaderMap;
 pub use method::Method;
+pub use request::Request;
 pub use status::StatusCode;
 pub use version::Version;
 pub use uri::Uri;
