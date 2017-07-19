bccc5817d3bcbf03060abc5fcc82d72f7195fc26
Without Boats <woboats@gmail.com>
2017-07-19 12:55:50 -0700

    Add Version::from_bytes method.

--- src/version.rs
+++ src/version.rs
@@ -29,7 +29,24 @@ enum Http {
     H2c,
 }
 
+/// A possible error value when converting `Version` from bytes.
+#[derive(Debug)]
+pub struct FromBytesError {
+    _priv: (),
+}
+
 impl Version {
+    /// Converts a slice of bytes to an HTTP version.
+    pub fn from_bytes(bytes: &[u8]) -> Result<Version, FromBytesError> {
+        match bytes {
+            b"HTTP/0.9" => Ok(HTTP_09),
+            b"HTTP/1.0" => Ok(HTTP_10),
+            b"HTTP/1.1" => Ok(HTTP_11),
+            b"h2"       => Ok(HTTP_2),
+            b"h2c"      => Ok(HTTP_2C),
+            _           => Err(FromBytesError { _priv: (), }),
+        }
+    }
 }
 
 impl fmt::Display for Version {
