a9b4c997b1495524c7238e32e2c83b860931563f
Sean McArthur <sean.monstar@gmail.com>
2017-07-24 20:42:35 -0700

    add Extensions

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,9 @@ pub mod version;
 pub mod uri;
 
 mod byte_str;
+mod extensions;
 
+pub use extensions::Extensions;
 pub use header::HeaderMap;
 pub use method::Method;
 pub use request::Request;
@@ -22,3 +24,14 @@ pub use response::Response;
 pub use status::StatusCode;
 pub use version::Version;
 pub use uri::Uri;
+
+fn _assert_types() {
+    fn assert_send<T: Send>() {}
+    fn assert_sync<T: Sync>() {}
+
+    assert_send::<Request<()>>();
+    assert_send::<Response<()>>();
+
+    assert_sync::<Request<()>>();
+    assert_sync::<Response<()>>();
+}
