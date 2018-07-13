492c5cab47cb9c7949e9ce8082ac2e00ee18ebd4
Sean McArthur <sean@seanmonstar.com>
2018-07-13 11:58:03 -0700

    use a noop IdHasher instead of Fnv in Extensions

--- src/extensions.rs
+++ src/extensions.rs
@@ -1,11 +1,33 @@
 use std::any::{Any, TypeId};
 use std::collections::HashMap;
-use std::hash::BuildHasherDefault;
+use std::hash::{BuildHasherDefault, Hasher};
 use std::fmt;
 
-use fnv::FnvHasher;
+type AnyMap = HashMap<TypeId, Box<Any + Send + Sync>, BuildHasherDefault<IdHasher>>;
 
-type AnyMap = HashMap<TypeId, Box<Any + Send + Sync>, BuildHasherDefault<FnvHasher>>;
+// With TypeIds as keys, there's no need to hash them. They are already hashes
+// themselves, coming from the compiler. The IdHasher just holds the u64 of
+// the TypeId, and then returns it, instead of doing any bit fiddling.
+#[derive(Default)]
+struct IdHasher(u64);
+
+impl Hasher for IdHasher {
+    fn write(&mut self, _: &[u8]) {
+        unreachable!("TypeId calls write_u64");
+    }
+
+    #[inline]
+    fn write_u64(&mut self, id: u64) {
+        self.0 = id;
+    }
+
+    #[inline]
+    fn finish(&self) -> u64 {
+        self.0
+    }
+}
+
+
 
 /// A type map of protocol extensions.
 ///
