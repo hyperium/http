e93c972d6065a05c87dc044b31ca84bb505e2126
David Kellum <dek-oss@gravitext.com>
2020-12-08 10:20:14 -0800

    indexing CODE_DIGITS debug assertions and doc

--- src/status.rs
+++ src/status.rs
@@ -136,6 +136,13 @@ impl StatusCode {
         let offset = (self.0.get() - 100) as usize;
         let offset = offset * 3;
 
+        // Invariant: self has checked range [100, 999] and CODE_DIGITS is
+        // ASCII-only, of length 900 * 3 = 2700 bytes
+
+        #[cfg(debug_assertions)]
+        { &CODE_DIGITS[offset..offset+3] }
+
+        #[cfg(not(debug_assertions))]
         unsafe { CODE_DIGITS.get_unchecked(offset..offset+3) }
     }
 
