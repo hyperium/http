cfee263395ef6a1847df05cde115bd8331740805
davkor <david@adalogics.com>
2021-04-09 23:42:19 +0100

    Initial fuzzer set up.

---
+++ fuzz/src/fuzz_http.rs
@@ -0,0 +1,28 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use libfuzzer_sys::arbitrary::Arbitrary;
+use http::Request;
+use http::StatusCode;
+use http::Response;
+
+#[derive(Debug, Arbitrary)]
+struct HttpSpec {
+        uri: Vec<u8>,
+        header_name: Vec<u8>,
+        header_value: Vec<u8>,
+        status_codes: Vec<u8>,
+        response: Vec<u8>,
+}
+
+fuzz_target!(|inp: HttpSpec|  {
+    let _ = Request::builder()
+                    .uri(&inp.uri[..])
+                    .header(&inp.header_name[..], &inp.header_value[..])
+                    .body(());
+
+    let _ = Response::builder()
+                        .header(&inp.header_name[..], &inp.header_value[..])
+                        .body(&inp.response[..]);
+    let _ = StatusCode::from_bytes(&inp.status_codes[..]);
+});
