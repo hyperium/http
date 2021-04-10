2a745b9d8f4cad7b823045bf86a82dedfe0b085c
davkor <david@adalogics.com>
2021-04-10 11:05:27 +0100

    fuzzer cleanup.

--- fuzz/src/fuzz_http.rs
+++ fuzz/src/fuzz_http.rs
@@ -1,28 +1,27 @@
 #![no_main]
 
-use libfuzzer_sys::fuzz_target;
-use libfuzzer_sys::arbitrary::Arbitrary;
 use http::Request;
-use http::StatusCode;
 use http::Response;
+use http::StatusCode;
+use libfuzzer_sys::arbitrary::Arbitrary;
+use libfuzzer_sys::fuzz_target;
 
 #[derive(Debug, Arbitrary)]
 struct HttpSpec {
-        uri: Vec<u8>,
-        header_name: Vec<u8>,
-        header_value: Vec<u8>,
-        status_codes: Vec<u8>,
-        response: Vec<u8>,
+    uri: Vec<u8>,
+    header_name: Vec<u8>,
+    header_value: Vec<u8>,
+    status_codes: Vec<u8>,
 }
 
-fuzz_target!(|inp: HttpSpec|  {
+fuzz_target!(|inp: HttpSpec| {
     let _ = Request::builder()
-                    .uri(&inp.uri[..])
-                    .header(&inp.header_name[..], &inp.header_value[..])
-                    .body(());
+        .uri(&inp.uri[..])
+        .header(&inp.header_name[..], &inp.header_value[..])
+        .body(());
 
     let _ = Response::builder()
-                        .header(&inp.header_name[..], &inp.header_value[..])
-                        .body(&inp.response[..]);
+        .header(&inp.header_name[..], &inp.header_value[..])
+        .body(());
     let _ = StatusCode::from_bytes(&inp.status_codes[..]);
 });
