b8ddea7d21a415918ba3d1d40984ef5f1f3658e6
tottoto <tottotodev@gmail.com>
2024-02-17 05:05:14 +0900

    refactor(header): add comment and lint allowing to panic in const context workaround

--- src/header/value.rs
+++ src/header/value.rs
@@ -86,6 +86,12 @@ impl HeaderValue {
         let mut i = 0;
         while i < bytes.len() {
             if !is_visible_ascii(bytes[i]) {
+                // TODO: When msrv is bumped to larger than 1.57, this should be
+                // replaced with `panic!` macro.
+                // https://blog.rust-lang.org/2021/12/02/Rust-1.57.0.html#panic-in-const-contexts
+                //
+                // See the panics section of this method's document for details.
+                #[allow(clippy::no_effect)]
                 ([] as [u8; 0])[0]; // Invalid header value
             }
             i += 1;
