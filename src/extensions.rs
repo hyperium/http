91cc6e9a2b4b9150c3d5d4bd23cf311ed7b30016
tottoto <tottotodev@gmail.com>
2024-02-17 02:05:38 +0900

    refactor(lib): refactor redundant closure

--- src/extensions.rs
+++ src/extensions.rs
@@ -61,7 +61,7 @@ impl Extensions {
     /// ```
     pub fn insert<T: Clone + Send + Sync + 'static>(&mut self, val: T) -> Option<T> {
         self.map
-            .get_or_insert_with(|| Box::new(HashMap::default()))
+            .get_or_insert_with(Box::default)
             .insert(TypeId::of::<T>(), Box::new(val))
             .and_then(|boxed| boxed.into_any().downcast().ok().map(|boxed| *boxed))
     }
@@ -138,7 +138,7 @@ impl Extensions {
     ) -> &mut T {
         let out = self
             .map
-            .get_or_insert_with(|| Box::new(HashMap::default()))
+            .get_or_insert_with(Box::default)
             .entry(TypeId::of::<T>())
             .or_insert_with(|| Box::new(f()));
         (**out).as_any_mut().downcast_mut().unwrap()
