eb42b0e19c03cea300d290a2356eb7d22070c47b
tottoto <tottotodev@gmail.com>
2024-02-17 02:01:23 +0900

    refactor(uri): replace single string with character

--- src/uri/authority.rs
+++ src/uri/authority.rs
@@ -238,7 +238,7 @@ impl Authority {
     pub fn port(&self) -> Option<Port<&str>> {
         let bytes = self.as_str();
         bytes
-            .rfind(":")
+            .rfind(':')
             .and_then(|i| Port::from_str(&bytes[i + 1..]).ok())
     }
 
