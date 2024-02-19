63102bcd29fcd4a094cac6a4afb0af370ef1fcbe
tottoto <tottotodev@gmail.com>
2024-02-20 08:06:42 +0900

    chore(lib): remove importing prelude AsRef trait

--- src/method.rs
+++ src/method.rs
@@ -18,7 +18,6 @@
 use self::extension::{AllocatedExtension, InlineExtension};
 use self::Inner::*;
 
-use std::convert::AsRef;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::str::FromStr;
